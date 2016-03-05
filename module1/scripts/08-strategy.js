e8318543d8668b264bb81bd3c867e8cf6d1753ea
Spark Liu <spak.liu.1984@gmail.com>
2016-03-05 23:23:17 +0800

    Add another strategy sample

--- module1/scripts/08-strategy.js
+++ module1/scripts/08-strategy.js
@@ -27,3 +27,27 @@
     };
     jack.speak();
 })();
+
+// Use strategy as parameter
+(function() {
+    console.log('-------------------------Example on strategy as parameter-------------------------');
+
+    var candidates = [{ name: 'Jack', charm: 100, money: 10 }, { name: 'Cal', charm: 40, money: 1000 }];
+
+    var rosePriority = candidates.sort(function(one, another) {
+        return one.charm - another.charm;
+    });
+    console.log('Rose chooses: ' + rosePriority[rosePriority.length - 1].name);
+
+    var anotherNobleGirlPriority = candidates.sort(function(one, another) {
+        return one.money - another.money;
+    });
+    console.log('Another noble girl chooses: ' + anotherNobleGirlPriority[anotherNobleGirlPriority.length - 1].name);
+
+    var analyzerPriority = candidates.sort(function(one, another) {
+        var oneValue = one.money * 0.3 + one.charm * 0.7;
+        var anotherValue = another.money * 0.3 + another.charm * 0.7;
+        return oneValue - anotherValue;
+    });
+    console.log('An analyzer chooses: ' + analyzerPriority[analyzerPriority.length - 1].name);
+})();

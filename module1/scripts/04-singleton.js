bf30c169a390d5dbdcef767f9ad273a3c082f29a
Spark Liu <spak.liu.1984@gmail.com>
2016-03-05 21:23:45 +0800

    Add sample of singleton by overriding new

--- module1/scripts/04-singleton.js
+++ module1/scripts/04-singleton.js
@@ -8,8 +8,13 @@
 
         function createInstance() {
             return {
-                shine: function shine() {
+                shiningCount: 0,
+                shine: function() {
                     console.log('Sun is shining ...');
+                    this.shiningCount++;
+                },
+                getShiningCount: function() {
+                    return this.shiningCount;
                 }
             };
         }
@@ -28,4 +33,41 @@
     var anotherSun = Sun.getInstance();
     console.log('Are there two suns: ' + (sun !== anotherSun));
     sun.shine();
+    sun.shine();
+    console.log('Count in the other sun: ' + anotherSun.getShiningCount());
+})();
+
+// Singleton by overriding new operator
+(function() {
+    console.log('-------------------------Example on singleton by overriding new-------------------------');
+    var Sun = (function() {
+        var sunInstance;
+
+        function createInstance() {
+            return {
+                shiningCount: 0,
+                shine: function() {
+                    console.log('Sun is shining ...');
+                    this.shiningCount++;
+                },
+                getShiningCount: function() {
+                    return this.shiningCount;
+                }
+            };
+        }
+
+        return function() {
+            if (!sunInstance) {
+                sunInstance = createInstance();
+            }
+            return sunInstance;
+        };
+    })();
+
+    var sun = new Sun();
+    var anotherSun = new Sun();
+    console.log('Are there two suns: ' + (sun !== anotherSun));
+    sun.shine();
+    sun.shine();
+    console.log('Count in the other sun: ' + anotherSun.getShiningCount());
 })();
